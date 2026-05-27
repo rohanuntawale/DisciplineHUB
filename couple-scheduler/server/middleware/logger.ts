c88d2637e84001028dd7b52edfc6c412e5da1846
Rohan Untawale <untawalerohan@gmail.com>
2026-05-27 20:42:34 +0530

    Reconstructed architecture

--- couple-scheduler/server/middleware/logger.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export const logger = (req: Request, _: Response, next: NextFunction) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
-  next();
-};
